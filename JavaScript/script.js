e985de5e8d3ba259094364f79ae386f2d166593f
Farkhodoff21 <farkhodoffcodes@gmail.com>
2022-07-09 18:38:16 +1400

    initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,19 @@
+//  Add hovered class in selected div list item
+let list = document.querySelectorAll('.navigation li');
+let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+
+toggle.onclick = function() {
+    navigation.classList.toggle('active')
+    main.classList.toggle('active')
+}
+
+function activeLink() {
+    list.forEach((item) => 
+        item.classList.remove('hovered'));
+        this.classList.add('hovered')
+    }
+    list.forEach((item) => 
+    item.addEventListener('mouseover', activeLink));
+    
